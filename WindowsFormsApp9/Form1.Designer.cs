dab9291ed71b82aad98ed64536ccef0e0a62ecf8
AriLopezM <ian.lopez31i@std.uni.edu.ni>
2024-06-16 22:04:14 -0600

    Add project files.

---
+++ WindowsFormsApp9/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace WindowsFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.lblIntervalo = new System.Windows.Forms.Label();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.pbrTrabajo = new System.Windows.Forms.ProgressBar();
+            this.tmrPrueba = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Font = new System.Drawing.Font("Lucida Fax", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValor.Location = new System.Drawing.Point(201, 32);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(63, 22);
+            this.lblValor.TabIndex = 0;
+            this.lblValor.Text = "Valor";
+            // 
+            // lblIntervalo
+            // 
+            this.lblIntervalo.AutoSize = true;
+            this.lblIntervalo.Font = new System.Drawing.Font("Lucida Fax", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIntervalo.Location = new System.Drawing.Point(201, 116);
+            this.lblIntervalo.Name = "lblIntervalo";
+            this.lblIntervalo.Size = new System.Drawing.Size(100, 22);
+            this.lblIntervalo.TabIndex = 1;
+            this.lblIntervalo.Text = "Intervalo";
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Font = new System.Drawing.Font("Lucida Fax", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicio.Location = new System.Drawing.Point(330, 32);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(130, 36);
+            this.btnInicio.TabIndex = 2;
+            this.btnInicio.Text = "Inicio";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click_1);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Font = new System.Drawing.Font("Lucida Fax", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetener.Location = new System.Drawing.Point(330, 109);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(130, 36);
+            this.btnDetener.TabIndex = 3;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // pbrTrabajo
+            // 
+            this.pbrTrabajo.Location = new System.Drawing.Point(179, 200);
+            this.pbrTrabajo.Name = "pbrTrabajo";
+            this.pbrTrabajo.Size = new System.Drawing.Size(308, 54);
+            this.pbrTrabajo.TabIndex = 4;
+            // 
+            // tmrPrueba
+            // 
+            this.tmrPrueba.Interval = 250;
+            this.tmrPrueba.Tick += new System.EventHandler(this.tmrPrueba_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbrTrabajo);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.lblIntervalo);
+            this.Controls.Add(this.lblValor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.Label lblIntervalo;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.ProgressBar pbrTrabajo;
+        private System.Windows.Forms.Timer tmrPrueba;
+        private EventHandler btnDetener_Click_1;
+    }
+}
+
---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        private int conteo;
+        public Form1()
+        {
+            InitializeComponent(); conteo = 0;
+        }
+
+        private void tmrPrueba_Tick_1(object sender, EventArgs e)
+        {
+            conteo++;
+            lblValor.Text = conteo.ToString();
+
+            if(pbrTrabajo.Value<100)
+                pbrTrabajo.Value++;
+            if(pbrTrabajo.Value==100)
+                tmrPrueba.Enabled = false;
+        }
+        private void btnInicio_Click_1(object sender, EventArgs e)
+        {
+            tmrPrueba.Enabled = true;
+        }
+        
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            tmrPrueba.Enabled = false;   
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            tmrPrueba.Enabled = false;
+        }
+
+    }
+}
+
+
+
